3cc0550c50986a77a2825b096692cfe1eff34259
3mk5mr <76589932+3mk5mr@users.noreply.github.com>
2021-02-08 00:45:17 +0200

    Update STONE.py

--- STONE.py
+++ STONE.py
@@ -32,3 +32,26 @@ if name == "STONE" :
     print("\033[0;32m8-install LEDAR-HACKING")
     print("\033[0;32m9-install BRUTEFORCEnew")
     print("\033[0;32m10-install A-Rat")
+else:
+    print("\033[0;31mwrong Password")
+stn = input (">>>")
+if stn ==  1  :
+    os.system('git clone https://github.com/rapid7/metasploit-framework')
+if stn ==  2  :
+    os.system('git clone https://github.com/ciku370/OSIF')
+if stn ==  3  :
+    os.system('git clone https://github.com/Bl4ckDr460n/HxWhatsApp')
+if stn ==  4  :
+    os.system('git clone https://github.com/lovehacker404/CameraHack/')
+if stn ==  5  :
+    os.system('git clone https://github.com/s0md3v/XSStrike')
+if stn ==  6  :
+    os.system('git clone https://github.com/kancotdiq/ipcs')
+if stn ==  7  :
+    os.system('git clone https://github.com/MoMuilWH/TmX-FW')
+if stn ==  8  :
+    os.system('git clone https://github.com/Ledear-Hacker/LEDEAR_HACKING')
+if stn ==  9  :
+    os.system('git clone https://github.com/FR13ND8/BRUTEFORCEnew')
+if stn ==  10  :
+    os.system('git clone https://github.com/RexTheGod/A-Rat')
